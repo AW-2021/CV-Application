b3919d8bd5b4f750d77652c0892c787fd187f145
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2025-05-21 00:31:34 +0500

    Modified layout of PersonalInfo.jsx component

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -14,45 +14,64 @@ const PersonalInfo = ({ disabledVal, inputs, handleChange }) => {
         name="jobTitle"
         value={inputs.jobTitle}
         onChange={handleChange}
-        placeholder="Software Developer"
+        placeholder="Junior Software Developer"
         className="block text-center text-lg m-auto"
       />
-      <div className="flex justify-center items-center">
-        <input
-          type="email"
-          id="emailAddress"
-          name="emailAddress"
-          value={inputs.emailAddress}
-          onChange={handleChange}
-          placeholder="aminawasif44@outlook.com"
-          className="w-56 text-center"
-        />|
-        <input
-          type="tel"
-          id="mobile"
-          name="mobile"
-          value={inputs.mobile}
-          onChange={handleChange}
-          placeholder="+90 6002135"
-          className="w-28 text-center"
-        />|
-        <input
-          id="city"
-          name="city"
-          value={inputs.city}
-          onChange={handleChange}
-          placeholder="Boston"
-          className="w-28 text-right"
-        />,
-        <input
-          id="country"
-          name="country"
-          value={inputs.country}
-          onChange={handleChange}
-          placeholder="USA"
-          className="w-20"
-        />
-      </div>
+      <table className="w-full">
+        <tbody>
+          
+          <tr className="flex justify-center items-center">
+            <td className="border-r-2 border-black px-3">
+              <input
+                type="email"
+                id="emailAddress"
+                name="emailAddress"
+                value={inputs.emailAddress}
+                onChange={handleChange}
+                placeholder="aminawasif@outlook.com"
+                className="max-w-56 text-center"
+              />
+            </td>
+
+            <td className="border-r-2 border-black px-3">
+              <input
+                type="tel"
+                id="mobile"
+                name="mobile"
+                value={inputs.mobile}
+                onChange={handleChange}
+                placeholder="+90 6002135"
+                className="max-w-32 text-center"
+              />
+            </td>
+
+            <td className="pl-3 text-center">
+              <input
+                id="city"
+                name="city"
+                value={inputs.city}
+                onChange={handleChange}
+                placeholder="Boston"
+                className="max-w-20"
+              />
+            </td>
+
+            <td>,</td>
+
+            <td className="px-3 text-center">
+              <input
+                id="country"
+                name="country"
+                value={inputs.country}
+                onChange={handleChange}
+                placeholder="USA"
+                className="max-w-20"
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
     </fieldset>
   );
 };
