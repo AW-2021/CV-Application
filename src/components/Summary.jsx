70a075c9949b4b9a67e69d7c76fac979aedd837c
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2025-05-21 00:29:39 +0500

    Adjusted CSS styling of Summary.jsx component

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,14 +1,14 @@
 
 const Summary = ({ disabledVal, inputs, handleChange }) => {
   return (
-    <fieldset className="border-b-2" disabled={disabledVal}>
+    <fieldset className="border-b-2 pb-2" disabled={disabledVal}>
         <legend className="font-bold">SUMMARY</legend>
         <input
           id="summary"
           name="summary"
           value={inputs.summary}
           onChange={handleChange}
-          className="w-full pt-2 pb-2 focus:pl-2 focus:pr-2"
+          className="w-full focus:p-2"
         />
     </fieldset>
   );
