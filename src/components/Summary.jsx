35342d40548d4911f9580b3a6733f2e6a436d9b5
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2025-05-21 15:27:39 +0500

    Added textarea and a new state with event handler function to Summary.jsx

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,17 +1,28 @@
+import { useState } from "react";
 
 const Summary = ({ disabledVal, inputs, handleChange }) => {
+  const [rowNumber, setRowNumber] = useState(1);
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      setRowNumber((prevNumber) => prevNumber + 1);
+    } 
+  }
+
   return (
     <fieldset className="border-b-2 pb-2" disabled={disabledVal}>
-        <legend className="font-bold">SUMMARY</legend>
-        <input
-          id="summary"
-          name="summary"
-          value={inputs.summary}
-          onChange={handleChange}
-          className="w-full focus:p-2"
-        />
+      <legend className="font-bold">SUMMARY</legend>
+      <textarea
+        id="summary"
+        name="summary"
+        rows={rowNumber}
+        value={inputs.summary}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+        className="w-full overflow-y-hidden resize-none focus:p-2"
+      ></textarea>
     </fieldset>
   );
-}
+};
 
 export default Summary;
