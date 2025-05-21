b2dd622a0c341ffcbd625f336523df5334f1a852
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2025-05-21 18:50:03 +0500

    Removed state and event handler function from Summary.jsx. Added field-sizing to textarea instead.

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,24 +1,14 @@
 import { useState } from "react";
 
 const Summary = ({ disabledVal, inputs, handleChange }) => {
-  const [rowNumber, setRowNumber] = useState(3);
-
-  const handleKeyDown = (event) => {
-    if (event.key === 'Enter') {
-      setRowNumber((prevNumber) => prevNumber + 1);
-    }
-  }
-
   return (
     <fieldset className="border-b-2 pb-2" disabled={disabledVal}>
       <legend className="font-bold">SUMMARY</legend>
       <textarea
         id="summary"
         name="summary"
-        rows={rowNumber}
         value={inputs.summary}
         onChange={handleChange}
-        onKeyDown={handleKeyDown}
         className="w-full overflow-y-hidden resize-none text-justify focus:p-2"
         placeholder="I am a Senior Product designer at CreativeMinds, creating meaningful, user-centered experiences. With a decade of design experience and a passion for pushing the boundaries of design. I design products that are not only beautiful but also functional and profitable."
       ></textarea>
