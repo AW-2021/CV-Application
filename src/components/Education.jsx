a0d809259f2e0f5606e6cadc61c834154efc73a6
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2025-05-27 17:42:47 +0500

    Created Education.jsx

---
+++ src/components/Education.jsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+
+const Education = ({ itemIndex, education, setEducation, handleChange }) => {
+  const [newEduDesc, setNewEduDesc] = useState("");
+
+  const handleAddDesc = (e, index) => {
+    const { name, value } = e.target;
+
+    if (e.key === "Enter") {
+      const newValue = { id: Date.now(), text: value };
+
+      setEducation((prevEducation) => {
+        const newEducation = [...prevEducation];
+        newEducation[index] = {
+          ...newEducation[index],
+          [name]: [...prevEducation[index]?.eduDescList || [], newValue],
+        }
+        return newEducation;
+      })
+      setNewEduDesc("");
+    }
+  };
+
+  return (
+    <>
+      <div className="w-full flex justify-between">
+        <input
+          name="educationInstitute"
+          value={education[itemIndex]?.educationInstitute || ""}
+          onChange={(e) => handleChange(e, itemIndex)}
+          className="font-bold uppercase"
+          placeholder="Brown University"
+        />
+        <input
+          name="educationLocation"
+          value={education[itemIndex]?.educationLocation || ""}
+          onChange={(e) => handleChange(e, itemIndex)}
+          className="font-bold"
+          placeholder="Rhode Island, USA"
+        />
+      </div>
+      <div className="w-full flex justify-between">
+        <div>
+          <input
+            name="educationDegree"
+            value={education[itemIndex]?.educationDegree || ""}
+            onChange={(e) => handleChange(e, itemIndex)}
+            className="font-bold"
+            placeholder="Bachelor of Industrial Design"
+          />
+          ,{" "}
+          <input
+            name="educationGrade"
+            value={education[itemIndex]?.educationGrade || ""}
+            onChange={(e) => handleChange(e, itemIndex)}
+            className="font-bold"
+            placeholder="CGPA 3.70 / 4.00"
+          />
+        </div>
+
+        <div>
+          <input
+            name="eduStartDate"
+            value={education[itemIndex]?.eduStartDate || ""}
+            onChange={(e) => handleChange(e, itemIndex)}
+            placeholder="Aug 2011"
+            className="font-bold"
+          />{" "}
+          -{" "}
+          <input
+            name="eduEndDate"
+            value={education[itemIndex]?.eduEndDate || ""}
+            onChange={(e) => handleChange(e, itemIndex)}
+            className="font-bold"
+            placeholder="May 2015"
+          />
+        </div>
+      </div>
+
+      <ul className="list-inside list-disc w-full">
+        {education[itemIndex]?.eduDescList?.map((desc) => (
+          <li key={desc.id}>{desc.text}</li>
+        ))}
+        <li>
+          <input
+            name="eduDescList"
+            value={newEduDesc}
+            onChange={(e) => setNewEduDesc(e.target.value)}
+            onKeyDown={(e) => handleAddDesc(e, itemIndex)}
+            className="w-11/12 focus:p-2"
+            placeholder="Write your academic achievements here."
+          />
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default Education;
