8eaeda21b47006dd7eda66752a874332dd6c45a0
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2025-05-28 21:54:13 +0500

    Added an overflow container as new CV page to Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,11 +3,26 @@ import PersonalInfo from "./PersonalInfo";
 import Summary from "./Summary";
 import EducationSection from "./EducationSection";
 import ExperienceSection from "./ExperienceSection";
+import Experience from "./Experience";
 
 const Form = ({ disabledVal }) => {
   const [inputs, setInputs] = useState({});
+  const [experiences, setExperiences] = useState([
+    {
+      id: 0,
+      companyName: "",
+      companyLocation: "",
+      companyRole: "",
+      jobType: "",
+      expStartDate: "",
+      expEndDate: "",
+      expDescList: [],
+    },
+  ]);
 
-  const handleChange = (e) => {
+  const [overflowItems, setOverflowItems] = useState([]);
+
+  const handleInputChange = (e) => {
     const { name, value } = e.target;
 
     setInputs((prevInputs) => ({
@@ -16,28 +31,57 @@ const Form = ({ disabledVal }) => {
     }));
   };
 
+  const handleExpChange = (e, index) => {
+    const { name, value } = e.target;
+
+    const newExperience = [...experiences];
+    newExperience[index] = {
+      ...newExperience[index],
+      [name]: value,
+    };
+    setExperiences(newExperience);
+  };
+
   return (
-    <form
-      autoComplete="off"
-      className="h-[29.7cm] w-[21cm] p-5 flex flex-col gap-4 bg-white rounded-sm overflow-y-hidden"
-    >
-      <PersonalInfo
-        disabledVal={disabledVal}
-        inputs={inputs}
-        handleChange={handleChange}
-      />
-      <Summary
-        disabledVal={disabledVal}
-        inputs={inputs}
-        handleChange={handleChange}
-      />
-      <EducationSection
-        disabledVal={disabledVal}
-      />
-      <ExperienceSection
-        disabledVal={disabledVal}
-      />
-    </form>
+    <div>
+      <form
+        autoComplete="off"
+        className="h-[29.7cm] w-[21cm] p-5 flex flex-col gap-4 bg-white rounded-sm overflow-y-hidden"
+      >
+        <PersonalInfo
+          disabledVal={disabledVal}
+          inputs={inputs}
+          handleChange={handleInputChange}
+        />
+        <Summary
+          disabledVal={disabledVal}
+          inputs={inputs}
+          handleChange={handleInputChange}
+        />
+        <EducationSection disabledVal={disabledVal} />
+        <ExperienceSection
+          disabledVal={disabledVal}
+          experiences={experiences}
+          setExperiences={setExperiences}
+          handleChange={handleExpChange}
+          setOverflowItems={setOverflowItems}
+        />
+      </form>
+      
+      <div className="h-[29.7cm] w-[21cm] mt-4 p-5 flex flex-col gap-4 bg-white rounded-sm overflow-y-hidden">
+        <div className="bg-green-600">
+          {overflowItems.map((item) => (
+            <Experience
+              key={item.id}
+              itemIndex={item.id}
+              experiences={experiences}
+              setExperiences={setExperiences}
+              handleChange={handleExpChange}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
   );
 };
 
