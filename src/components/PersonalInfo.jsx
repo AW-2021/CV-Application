d0fc712ea4523d30e8f3b4b9b01417007e397ec9
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2025-05-31 02:53:03 +0500

    Bug fix on resizing after deployment

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -17,7 +17,7 @@ const PersonalInfo = ({ disabledVal, inputs, handleChange }) => {
         placeholder="Senior Product Designer"
         className="w-full block text-center text-lg m-auto mb-2"
       />
-      <table className="w-4/5 m-auto">
+      <table className="md:w-4/5 w-full m-auto">
         <tbody>
           <tr className="text-sm text-center">
             <td className="w-1/3 border-r-2 border-black py-0">
@@ -51,7 +51,7 @@ const PersonalInfo = ({ disabledVal, inputs, handleChange }) => {
                 value={inputs.city}
                 onChange={handleChange}
                 placeholder="San Francisco"
-                className="w-[55%] text-right"
+                className="md:w-[55%] w-[65%] text-right"
               />{", "}
               <input
                 id="country"
@@ -59,7 +59,7 @@ const PersonalInfo = ({ disabledVal, inputs, handleChange }) => {
                 value={inputs.country}
                 onChange={handleChange}
                 placeholder="USA"
-                className="w-[40%]"
+                className="md:w-[40%] w-[25%]"
               />
             </td>
           </tr>
