e5095447774eab2c166b1470aceccd046852230b
Amina Wasif <77668986+AW-2021@users.noreply.github.com>
2025-05-31 02:08:54 +0500

    Moved form and form section states up to their common App parent

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,57 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Form from "./components/Form";
+import CV from "./components/pdf/CV";
 
 const App = () => {
   const [disabledVal, setDisabledVal] = useState("disabled");
 
+  const [inputs, setInputs] = useState({});
+  const [educations, setEducations] = useState([
+    {
+      id: 0,
+      educationInstitute: "",
+      educationLocation: "",
+      educationDegree: "",
+      educationGrade: "",
+      eduStartDate: "",
+      eduEndDate: "",
+      eduDescList: [],
+    },
+  ]);
+  const [experiences, setExperiences] = useState([
+    {
+      id: 0,
+      companyName: "",
+      companyLocation: "",
+      companyRole: "",
+      jobType: "",
+      expStartDate: "",
+      expEndDate: "",
+      expDescList: [],
+    },
+  ]);
+
   return (
     <div className="w-full h-screen bg-blue-100">
-      <Header setDisabledVal={setDisabledVal} />
+      <Header
+        setDisabledVal={setDisabledVal}
+        inputs={inputs}
+        educations={educations}
+        experiences={experiences}
+      />
       <div className="flex justify-center pt-44 pb-14 bg-blue-100">
-        <Form disabledVal={disabledVal} />
+        <Form
+          disabledVal={disabledVal}
+          inputs={inputs}
+          setInputs={setInputs}
+          educations={educations}
+          setEducations={setEducations}
+          experiences={experiences}
+          setExperiences={setExperiences}
+        />
       </div>
+      {/* <CV inputs={inputs} educations={educations} experiences={experiences} /> */}
     </div>
   );
 };
